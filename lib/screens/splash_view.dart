9f18dde15d334e1ee5fef042c0508fa448e55c43
tolgasozbir <tolgasoz1@gmail.com>
2022-07-29 23:59:32 +0300

    changed splash screen background color

--- lib/screens/splash_view.dart
+++ lib/screens/splash_view.dart
@@ -41,6 +41,7 @@ class _SplashViewState extends State<SplashView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(child: _bodyView()),
     );
   }
